3684ccdc995b2a2ad36375745b85293fc3817555
RW-53\rw53 <zo4j4g4ji3@gmail.com>
2021-03-19 17:32:13 +0800

    颱風API

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -5,3 +5,27 @@ fetch("https://ocean.taiwan.gov.tw/OacGA_HF/ocatwgatotalpageviews.json")
   let pageViews = result.pageviews
   $('#pageViews').text(pageViews);
 })
+
+fetch("https://ocean.taiwan.gov.tw/OpenData/CWB_Typhoon/json/W-C0034-001_002.json")
+.then(res => {
+  console.log(res);
+  return res.json();
+}).then(data => {
+  console.log(data);
+  console.log(data.cap)
+  let output = document.getElementById('marquee').innerHTML
+  data.cap.forEach(function(post) {
+    output += `
+    <div class="py-1 px-2 rounded-pill">
+    ${post.description}
+    </div>
+    `
+    //output.replace(/\。/g,'。<br>');
+    var a = typeof(post.description.replace(/\。/g,'。<br>'));
+    console.log(a);
+    console.log(post.description);
+    
+    document.getElementById('marquee').innerHTML = output;
+    console.log(output);
+  });
+})
